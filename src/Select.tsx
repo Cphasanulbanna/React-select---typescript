bc8e49cf0b18a1b7529335615c4a7aa3a474fcc2
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-18 15:18:28 +0530

    dropdown click functionality

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import styles from "./select.module.css"
 
 type SelectOption= {
@@ -12,13 +13,14 @@ type SelectProps = {
 }
 
 const Select = ({value,onChange,options}: SelectProps) => {
+    const [open,setOpen] = useState(false)
   return (
-    <div tabIndex={0} className={styles.container}>
+    <div onClick={() => setOpen((prev) => !prev)} tabIndex={0} className={styles.container}>
         <span className={styles.value}>{value?.label}</span>
         <button className={styles["clear-btn"]}>&times;</button>
         <div className={styles.divider}></div>
         <div className={styles.caret}></div>
-        <ul className={`${styles.options} ${styles.show}`}>
+        <ul className={`${styles.options} ${open ? styles.show : ""}`}>
             {options?.map((option) => {
                 return <li key={option.label} className={styles.option}>{option.label}</li>
             })}
